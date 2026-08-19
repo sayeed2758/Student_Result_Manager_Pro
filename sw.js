64dec762d3b4e99601e0099291909ab1e0596155
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-19 22:44:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='ezee-result-manager-v5';
+const CACHE='ezee-result-manager-v6';
 const ASSETS=['./','./index.html','./app.js','./assets/style.css','./assets/logo.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('message',e=>{if(e.data==='SKIP_WAITING')self.skipWaiting()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(x=>x.put(e.request,cp));return r}).catch(()=>caches.match('./index.html'))))});
-
 
